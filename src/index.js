6922ca1b858f882069a299aa3c2e63aba0080348
Jahnavi_Gadde <gaddeyjahnavi@gmail.com>
2023-07-11 19:47:39 +0530

    4

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import './style.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>
+    React.StrictMode  basename={process.env.APP_URL}>
+    <BrowserRouter>
+    <App / >
+    </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
