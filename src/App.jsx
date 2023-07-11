73c1721b2d56e1696e89d7758994069739e921ed
Jahnavi_Gadde <gaddeyjahnavi@gmail.com>
2023-07-11 19:28:20 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
     return (
         <div className = "App" >
         <div className = "child" >
-         <BrowserRouter>
          <CollapsibleExample/>
+         <BrowserRouter>
          <Route exact path='/' render= {<Home/>} />
         </BrowserRouter>
         </div>
